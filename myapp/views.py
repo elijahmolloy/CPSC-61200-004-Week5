4f984d262d4592f7cbdd1bfb4ca5d4c97959025d
Elijah Molloy <elijahmolloy@gmail.com>
2023-04-15 15:12:42 -0600

    Addition of first HelloWorld view

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,10 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
+TEMPLATE_DIRS = (
+    'os.path.join(BASE_DIR, "templates")'
+)
+
 # Create your views here.
+def index(request):
+    return render(request, "index.html")
