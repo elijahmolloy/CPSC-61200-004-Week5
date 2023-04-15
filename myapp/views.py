5829c5a6abef2ae90a3602848a723bc72233598b
Elijah Molloy <elijahmolloy@gmail.com>
2023-04-15 15:15:34 -0600

    Pass variables to main view

--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+import datetime
 
 TEMPLATE_DIRS = (
     'os.path.join(BASE_DIR, "templates")'
@@ -7,4 +8,8 @@ TEMPLATE_DIRS = (
 
 # Create your views here.
 def index(request):
-    return render(request, "index.html")
+    variables = {
+        "current_date": datetime.datetime.now().date()
+    }
+    
+    return render(request, "index.html", variables)
